cd120f4026a105903e4b31c7305f0022d43d0b74
Geoffrey Vander Cruyssen <geoffrey.vandercruyssen@gmail.com>
2021-09-29 21:10:54 +0200

    Add e2e story

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,28 @@
 from selenium import webdriver
 
 browser = webdriver.Firefox()
+
+# A user goes to the homepage to check out the website
 browser.get("http://localhost:8000")
 
-assert 'Django' in browser.title
+# The user notices the page title and header mention to-do lists
+assert 'To-Do' in browser.title
+
+# The user is invited to enter a to-do item straight away
+
+# The user types "Buy peacock feathers" into a text box
+
+# When the user hits enter, the page updates and now the page lists
+# "1: Buy peacock feathers" as an item in a to-do list
+
+# There is still a text box inviting the user to add another item. The user enters 
+# "Use peacock feathers to make a fly"
+
+# The page updates again, and now shows both items on their list
+
+# The user sees a unique url and some explanatory message about saving the list
+
+# The user visits the url and the to-do list is still there
+
+# The user quits the application
+browser.quit()
